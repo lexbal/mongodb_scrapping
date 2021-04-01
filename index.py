342c641b8d56c3a136c01462ce7267df631dc66c
root <alexballe35@gmail.com>
2021-04-01 16:01:48 +0200

    update flask

--- index.py
+++ index.py
@@ -13,7 +13,24 @@ app.config.from_object('config')
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    nb_airport_per_continent = db.airports.aggregate([
+        {"$group": {"_id": "$continent", "count": {"$sum": 1}}},
+        {"$sort": {"_id": 1}}
+    ])
+
+    airports_by_score = db.airports.aggregate([
+        {"$group": {
+            "_id": "$continent", 
+            "name" : {"$first" : "$name"}, 
+            "country" : {"$first" : "$country"}, 
+            "maxScore": { "$max": "$score" }
+        }},
+        {"$sort": {"maxScore": -1}}
+    ])
+
+    return render_template('index.html',
+                            nb_airport_per_continent=nb_airport_per_continent,
+                            airports_by_score=airports_by_score)
 
 if __name__ == "__main__":
     app.run()
