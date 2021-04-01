9ed556571b6d5e259466ff8697fa28b032f1f200
root <alexballe35@gmail.com>
2021-04-01 12:09:53 +0200

    add flask base

---
+++ index.py
@@ -0,0 +1,18 @@
+import locale
+
+from flask import Flask, render_template
+from pymongo import MongoClient
+import pymongo
+
+app = Flask(__name__)
+
+app.config.from_object('config')
+
+
+@app.route('/')
+def index():
+
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    app.run()
