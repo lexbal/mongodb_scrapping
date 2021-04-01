e0309e0338ebf56ec13c372181616ea2357e9bb1
root <alexballe35@gmail.com>
2021-04-01 12:24:25 +0200

    adding mongo class

--- index.py
+++ index.py
@@ -1,17 +1,18 @@
 import locale
+import pymongo
 
 from flask import Flask, render_template
 from pymongo import MongoClient
-import pymongo
+from mongo_db import Mongo;
 
-app = Flask(__name__)
+mongo = Mongo()
+db    = mongo.get_database()
+app   = Flask(__name__)
 
 app.config.from_object('config')
 
-
 @app.route('/')
 def index():
-
     return render_template('index.html')
 
 if __name__ == "__main__":
